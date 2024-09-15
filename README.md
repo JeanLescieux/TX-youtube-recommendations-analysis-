03c4f262cb2758206bb20c0bd13d5198cc2c8f6d
@jlescieu <jean.lescieu@etu.utc.fr>
2024-09-15 15:21:59 +0200

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # TX-youtube-recommendations-analysis
+
+Installer Firefox
+Aller dans about:debugging/Ce Firefox/Charger un module complémentaire temporaire..
+Ajouter le fichier manifest.json
+Faire une navigation youtube classique
--- content.js
+++ content.js
@@ -1,5 +1,4 @@
 const getVideoTitle = () => {
-    // Recherche du titre de la vidéo à partir de l'élément DOM spécifique
     const titleElement = document.querySelector('h1.title yt-formatted-string');
     return titleElement ? titleElement.innerText : null;
   };
@@ -7,15 +6,17 @@ const getVideoTitle = () => {
   const checkForVideoTitle = () => {
     const videoTitle = getVideoTitle();
     if (videoTitle) {
-      console.log(`Video title found: ${videoTitle}`);  // Log pour vérifier si le titre est récupéré
-      browser.runtime.sendMessage({ videoTitle });
+      console.log(`Video title found: ${videoTitle}`);  // Vérifier si le titre est récupéré
+      browser.runtime.sendMessage({ videoTitle }).then(() => {
+        console.log("Message sent to background script");
+      }).catch(err => {
+        console.error("Error sending message:", err);
+      });
     } else {
       console.log("No video title found, retrying...");
-      // Réessayer après un certain temps si le titre n'est pas encore disponible
       setTimeout(checkForVideoTitle, 1000);
     }
   };
   
-  // Lancer la vérification
   checkForVideoTitle();
   
